95bf1bed4e76bc144ccedd8be126d4b81cf3d09f
Kawsar <web.kawsarahed@gmail.com>
2024-01-16 19:39:36 +0600

    practice associative array

--- array.php
+++ array.php
@@ -15,7 +15,7 @@
         // echo $name.$salary.$profession;
 
         $person = array('Samim Mohammad', 15000, "Pilot");
-        var_dump($person);
+        // var_dump($person);
 
         // array er vitore coma separate kore rakhte hobe
 
@@ -23,6 +23,50 @@
         // null o hochhe ek dorer data type
         // resource o ek dorer data type jekhane onek gulo reference thake
         // like: database
+
+
+        // array
+        // indexed array
+        // associative array
+        // multi dimensional array
+
+
+        // // indexed array
+        $teams = array("Samim", "Kawsar", "Mustakim", "Ruhi");
+        var_dump($teams);
+        echo "</br>";
+
+        print_r($teams);
+        echo "</br>";
+
+        $length = count($teams);
+        echo $length;
+        echo "</br>";
+
+        foreach($teams as $name) {
+            echo "Team member: ".$name."</br>";
+        }
+
+
+        // associative array
+        $profile = array(
+            "name" => "Kawsar Ahmed",
+            "age" => 27,
+            "profession" => "Developer"
+        );
+        var_dump($profile);
+        echo "</br>";
+
+        print_r($profile);
+        echo "</br>";
+
+        $length = count($profile);
+        echo $length;
+        echo "</br>";
+
+        foreach($profile as $key => $value) {
+            echo "profile $key = $value </br>";
+        }
 
     ?>
 </body>
