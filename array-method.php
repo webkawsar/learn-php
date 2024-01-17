9a0e52353678e97ee45e599181e008e48a43f125
Kawsar <web.kawsarahed@gmail.com>
2024-01-17 10:40:18 +0600

    practice array chunk, array column, array combine, array key case changing

--- array-method.php
+++ array-method.php
@@ -29,7 +29,7 @@
 
 
         // associative array sorting
-        $profile = array("name" => "Kawsar Ahmed", "profession" => "Developer", "job" => "Bekar");
+        // $profile = array("Name" => "Kawsar Ahmed", "profession" => "Developer", "job" => "Bekar");
 
         // sorted by ascending
         // asort($profile);
@@ -41,12 +41,53 @@
         // ksort($profile) ;
 
         // sorted key in descending
-        krsort($profile);
+        // krsort($profile);
 
-        foreach($profile as $key => $value) {
+        // foreach($profile as $key => $value) {
 
-            echo "$key = $value </br>";
-        }
+        //     echo "$key = $value </br>";
+        // }
+
+        
+        // change all key to lowercase
+        // $changed_key_case = array_change_key_case($profile);
+
+        // change all key to uppercase
+        // $changed_key_case = array_change_key_case($profile, CASE_UPPER);
+        
+        // changing array key case
+        // foreach($changed_key_case as $key => $value) {
+        //     echo "$key = $value </br>";
+        // }
+
+
+
+        // array chunk
+        // $divided_array = array_chunk($profile, 2);
+        // var_dump($divided_array);
+
+
+
+        // array column
+        // $teams = array(
+        //     array("id" => 1, "user_name" => "kawsar", 'email' => 'kawsarahmed.dev@gmail.com'),
+        //     array("id" => 2, "user_name" => "mustakim", 'email' => 'mustakim@gmail.com'),
+        //     array("id" => 3, "user_name" => "ramjan", 'email' => 'ramjan@gmail.com')
+        // );
+
+        // $keys = array_column($teams, 'email', 'user_name');
+        // print_r($keys);
+
+
+
+        // combine array
+        // 2ta indexed array ke associative array te transform kora jay
+        $ids = array(1, 2, 3);
+        $user_name = array('kawsar', 'samim', 'imran');
+        $combined_array = array_combine($user_name, $ids);
+
+        print_r($combined_array);
+
 
     ?>
 </body>
