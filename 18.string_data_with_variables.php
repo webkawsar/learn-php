96de34d5f7c8d4926c1c588c83b05008575eb493
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-02-08 19:49:55 +0600

    practice basic string data with variables

---
+++ 18.string_data_with_variables.php
@@ -0,0 +1,20 @@
+<?php
+
+    $name = "Kawsar Ahmed";
+    $age = 29;
+
+    echo $name;
+    echo $age;
+
+    echo "<br />";
+
+    echo "My name is $name and I am $age years old";
+    echo "<br />";
+    echo 'My name is $name and I am $age years old';
+    echo "<br />";
+    
+
+
+
+
+?>
---
+++ 19.math.php
@@ -0,0 +1,12 @@
+
+<?php
+
+    $alo = 120;
+    $potol = 40;
+    $mass = 400;
+
+    $mot = $alo + $potol + $mass;
+
+    echo "Total $mot takar bajar koriyasi";
+
+?>
