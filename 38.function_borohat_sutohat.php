8b9900ffc84e3690c7aad2f0dc125c49190a43a2
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-02-13 23:34:32 +0600

    practiced function borohat, sutohat

---
+++ 38.function_borohat_sutohat.php
@@ -0,0 +1,18 @@
+<?php
+
+
+function boroHat() {
+
+    return "<span style=\"text-transform: uppercase\">Hi, Mr. Kawsar</span>";
+}
+
+function sutoHat() {
+
+    return "<span style=\"text-transform: lowercase\">Hi, Mr. Kawsar</span>";
+}
+
+
+echo boroHat();
+echo sutoHat();
+
+
