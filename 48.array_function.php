75a0a9b52ed6041648371152d4723dab34e6969e
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-02-15 00:03:39 +0600

    practice array function

---
+++ 48.array_function.php
@@ -0,0 +1,25 @@
+<?php
+
+
+// array function/method
+
+$fruits = ['Apple', "Banana", "orange"];
+
+// get array length
+// echo count($fruits);
+
+
+// get first element
+// echo current($fruits);
+
+
+// get last element
+// echo end($fruits);
+
+
+// get second element
+echo next($fruits);
+
+
+
+
