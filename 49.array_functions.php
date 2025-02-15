38c940640e6e8eb0a9f78fb8a9d7e3683d81b273
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-02-15 19:41:10 +0600

    practice array push, pop, shift, unshift

---
+++ 49.array_functions.php
@@ -0,0 +1,44 @@
+<?php
+
+    $fruits = ['Apple', "Banana", "orange"];
+
+    // remove and return last element of array and modify main array
+    // echo array_pop($fruits);
+
+    // echo "<pre>";
+    // print_r($fruits);
+    // echo "</pre>";
+    // echo "<br />";
+
+
+    // push element end of the array and return count and modify main array
+    // echo array_push($fruits, 'Pineapple');
+
+    // echo "<pre>";
+    // print_r($fruits);
+    // echo "</pre>";
+    // echo "<br />";
+
+
+    // remove first element of array and return and modify main array
+    // echo array_shift($fruits);
+
+    // echo "<pre>";
+    // print_r($fruits);
+    // echo "</pre>";
+    // echo "<br />";
+
+
+    // added element in first index and return array count. modify main array
+    echo array_unshift($fruits, 'Pineapple');
+
+    echo "<pre>";
+    print_r($fruits);
+    echo "</pre>";
+    echo "<br />";
+
+
+
+
+
+
