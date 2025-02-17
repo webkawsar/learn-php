6fd884b3218db0806faf1c331d199a6bbdd03f7f
Kawsar <web.kawsarahmed@gmail.com>
2025-02-17 11:10:13 +0600

    practiced array reverse and array unique

---
+++ 50.array_function_3.php
@@ -0,0 +1,49 @@
+<?php
+
+
+    $fruits = ['Apple', "Banana", "Orange", "Apple", "Orange"];
+
+    // return reversed array but not modify main array
+    // $reversed_array = array_reverse($fruits);
+
+
+    // echo "<pre>";
+    // print_r($reversed_array);
+    // echo "</pre>";
+    // echo "<br />";
+
+    // echo "<pre>";
+    // print_r($fruits);
+    // echo "</pre>";
+    // echo "<br />";
+
+
+
+    // return a unique array but not modify main array
+    $unique_array = array_unique($fruits);
+
+    echo "<pre>";
+    print_r($unique_array);
+    echo "</pre>";
+    echo "<br />";
+
+    echo "<pre>";
+    print_r($fruits);
+    echo "</pre>";
+    echo "<br />";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
