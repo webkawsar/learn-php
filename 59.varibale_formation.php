152c361b9068df534f93b45945b278da72858adb
Kawsar <web.kawsarahmed@gmail.com>
2025-02-18 17:43:33 +0600

    practice undefine variable, flag variable, array variable

---
+++ 59.varibale_formation.php
@@ -0,0 +1,36 @@
+<?php
+
+    // undefine variable
+    // flag variable
+    // array variable
+
+
+
+    // undefine variable
+    $name;
+    echo $name;
+
+
+
+    // flag variable
+    $total = 0;
+
+    $total += 1;
+
+
+
+
+    // array variable
+    $friends[] = "Samim Mohammad";
+    $friends[] = "Imran Ahmed";
+    $friends[] = "Sadik Kayem";
+
+
+
+
+
+
+
+
+
+
