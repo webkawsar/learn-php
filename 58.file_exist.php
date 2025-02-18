77415b299c3fda35b442eeb8b56849c80f0ebf11
Kawsar <web.kawsarahmed@gmail.com>
2025-02-18 17:35:07 +0600

    file exist

---
+++ 58.file_exist.php
@@ -0,0 +1,16 @@
+<?php
+
+    // evabe condition diye require korle kuno karone jodi file na thake tahole Fatal Error show korbe na 
+    if(file_exists('19.math.php')) {
+        require_once('19.math.php');
+    }
+
+
+
+
+
+
+
+
+
+
