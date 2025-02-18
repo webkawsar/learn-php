824e14ebd60b3f31bfa2daa1e902d3412cd5b615
Kawsar <web.kawsarahmed@gmail.com>
2025-02-18 16:36:47 +0600

    practice implode

---
+++ 55.implode.php
@@ -0,0 +1,25 @@
+<?php
+
+    $text = "Hi, I am Kawsar, I am Dev, I also love Gorur Biriyani, Etai Bastob, Ha Ha";
+    
+    // return a array of string
+    $string_array = explode(',', $text);
+    
+    echo "<pre>";
+    print_r($string_array);
+    echo "</pre>";
+    echo "<br />";
+
+
+    // return a string
+    $text_string = implode('.', $string_array);
+
+    echo "<pre>";
+    print_r($text_string);
+    echo "</pre>";
+    echo "<br />";
+
+
+
+
+
