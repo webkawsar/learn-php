021f5b78255a50b61b056946c9db94636bc25e31
Kawsar <web.kawsarahmed@gmail.com>
2025-02-18 18:06:02 +0600

    isset, empty, die practice

---
+++ 60.isset_empty_die.php
@@ -0,0 +1,38 @@
+<?php
+
+    $value;
+
+    if(isset($value)) {
+        echo "Value ache";
+    } else {
+        echo "Value nai";
+        echo "<br />";
+    }
+
+
+    // empty check kore vairable er value ase kina and value empty kina
+    $name = "Kawsar";
+    if(empty($name)) {
+        echo "Name empty";
+    } else {
+        echo "Name not empty";
+    }
+
+
+
+    echo "I Love BD <br />";
+    echo "I Love BD <br />";
+    die();
+    echo "I Love BD <br />";
+    echo "I Love BD <br />";
+
+
+
+
+
+
+
+
+
+
+
