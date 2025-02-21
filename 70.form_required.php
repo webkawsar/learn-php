69523226ca8c9e1d801c53e55136ef665293132e
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-02-21 23:28:20 +0600

    practice submitted form error handling

---
+++ 70.form_required.php
@@ -0,0 +1,39 @@
+
+<?php
+
+/**
+ * get submitted form data here
+ */
+    if(isset($_POST['submit'])) {
+       
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $error = [];
+        if(empty($email)) {
+            $error['email'] = "Email is required";
+        }
+
+        if(empty($password)) {
+            $error['password'] = "Password is required";
+        }
+
+    }
+
+   
+?>
+
+<div>
+    
+    
+</div>
+
+<form action="" method="POST">
+    <input type="text" name="email" id="email" placeholder="Email"> <br>
+    <?php if(isset($error['email'])) echo $error['email'] ?><br>
+
+    <input type="password" name="password" id="password" placeholder="Password"> <br> <br>
+    <?php if(isset($error['password'])) echo $error['password'] ?><br>
+
+    <input type="submit" name="submit" value="Log In">
+</form>
