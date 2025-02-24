32d73a9dc65e4803a8a031441dc243ff1a737b7b
Kawsar <web.kawsarahmed@gmail.com>
2025-02-24 17:15:49 +0600

    practice array count function

---
+++ array-module/5.array_count.php
@@ -0,0 +1,24 @@
+<?php
+
+// array
+// index array
+// associative array
+// multidimensional array
+
+
+$friends = ["Kawsar", "Samim", "Imran", "Javed"];
+
+echo "<pre>";
+print_r($friends);
+echo "</pre>";
+echo "<br />";
+
+// return count of array
+echo count($friends);
+
+echo "<pre>";
+print_r($friends);
+echo "</pre>";
+echo "<br />";
+
+
