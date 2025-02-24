0cf9841f0fe2784786ca4fa95f967f4327885535
Kawsar <web.kawsarahmed@gmail.com>
2025-02-24 17:42:43 +0600

    practice array next function

---
+++ array-module/8.array_second_element.php
@@ -0,0 +1,28 @@
+<?php
+
+// array
+// index array
+// associative array
+// multidimensional array
+
+
+$friends = ["Kawsar", "Samim", "Imran", "Javed"];
+
+echo "<pre>";
+print_r($friends);
+echo "</pre>";
+echo "<br />";
+
+// return second element of array
+echo next($friends);
+echo "<br />";
+
+// return third element of array
+echo next($friends);
+
+echo "<pre>";
+print_r($friends);
+echo "</pre>";
+echo "<br />";
+
+
