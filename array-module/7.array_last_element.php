76a42c77987035157520686b4b2b31dfbac17eea
Kawsar <web.kawsarahmed@gmail.com>
2025-02-24 17:39:58 +0600

    practice array first and last element get function

---
+++ array-module/7.array_last_element.php
@@ -0,0 +1,24 @@
+<?php
+
+// array
+// index array
+// associative array
+// multidimensional array
+
+
+$friends = ["Kawsar", "Samim", "Imran", "Javed"];
+
+echo "<pre>";
+print_r($friends);
+echo "</pre>";
+echo "<br />";
+
+// return first element of array
+echo end($friends);
+
+echo "<pre>";
+print_r($friends);
+echo "</pre>";
+echo "<br />";
+
+
