492af154c00016cef52f5fa413f6ae9344a14906
Kawsar <web.kawsarahmed@gmail.com>
2025-02-25 19:22:09 +0600

    practice array sort

---
+++ array-module/15.array_some_method.php
@@ -0,0 +1,36 @@
+<?php
+
+    $alphabet = array('c', 'd', 'e', 'a', 'b');
+    
+    echo "<pre>";
+    print_r($alphabet);
+    echo "</pre>";
+    echo "<br />";
+
+    
+    // index array sorting by ascending and modify main array
+    echo sort($alphabet);
+
+    echo "<pre>";
+    print_r($alphabet);
+    echo "</pre>";
+    echo "<br />";
+
+
+
+    // index array sorting by descending and modify main array
+    $numbers = array(1, 3, 5, 4, 11, 50, 25, 500, 2500, 100);
+
+    echo "<pre>";
+    print_r($numbers);
+    echo "</pre>";
+    echo "<br />";
+    
+    // sort($numbers);
+    rsort($numbers);
+
+    echo "<pre>";
+    print_r($numbers);
+    echo "</pre>";
+    echo "<br />";
+
