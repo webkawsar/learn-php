6ab41ea200267a7fb28574aedfe829eeec5b391f
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-02-27 23:48:00 +0600

    practice array keys difference

---
+++ array-module/22.array_keys_count.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    // difference between two array by keys
+    // compare based on first array
+    $array1 = array("a" => 10, "b" => 20, "c" => 30, 'd' => 30, 'e' => 50);
+    $array2 = array('a' => 10, "b" => 20, "c" => 30, 'd' => 40, 'f' => 50);
+
+    $new_diff_array = array_diff_key($array1, $array2);
+
+    echo "<pre>";
+    print_r($new_diff_array);
+    echo "</pre>";
+    echo "<br />";
+
+
