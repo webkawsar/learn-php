15bb27d509a1d06e2e88debdafa700edee9ed94d
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-02-28 23:35:51 +0600

    practice array element fill method

--- array-module/index.php
+++ array-module/index.php
@@ -1,5 +1,17 @@
 <?php
 
+    // array element value fil up by text
+    // first parameter e index koto theke suru hobe bole dite hoy
+    // second parameter e koyta element add hobe bole dite hoy
+    // third parameter e element ki diye fill up hobe seta dite hoy
+    // syntax: array_fill(startIndex, how many element will be add, element text);
 
+    $created_array_with_value = array_fill(0, 5, 'Dummy Text');
+    var_dump($created_array_with_value);
+
+    echo "<pre>";
+    print_r($created_array_with_value);
+    echo "</pre>";
+    echo "<br />";
 
 
