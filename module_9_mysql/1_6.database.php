20985909420311ca5a48f55c269fa7f5ee80f4cc
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-03-04 22:14:34 +0600

    practice mysql basics

---
+++ module_9_mysql/1_6.database.php
@@ -0,0 +1,8 @@
+
+// kivabe database access korte hoy
+// database related some apps like: table Plus, designer.net etc.
+// kivabe database create korte hoy 
+// kivabe table create korte hoy 
+// kivabe table e data insert korte hoy 
+
+// 7. database e giye privileage e giye access username, password set korte hoy
---
+++ module_9_mysql/8.connection_statement.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$connection = new mysqli("host", "username", "password");
+
+// statement are two ways
+// $connection -> query();
+// $connection -> prepare();
