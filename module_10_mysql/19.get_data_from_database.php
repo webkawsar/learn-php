bc5a3bd2a089f0303e7f08b2077d187a2fa3340c
Kawsar <web.kawsarahmed@gmail.com>
2025-03-06 12:28:31 +0600

    practice get data from database by fetch

--- module_10_mysql/19.get_data_from_database.php
+++ module_10_mysql/19.get_data_from_database.php
@@ -14,9 +14,33 @@
 
     // fetch()
     // fetchAll()
+
+    // index & associative both data return
     // fetch_array()
+
+    // return only associative array data
     // fetch_assoc()
+
+    // return associative array data but data can access to different way
     // fetch_object()
+
+    // $user_data = $data -> fetch_assoc();
+
+    // echo "<pre>";
+    // print_r($user_data);
+    // echo "</pre>";
+    // echo "<br />";
+
+    // $object_data = $data -> fetch_object();
+    // echo $object_data -> name;
+
+
+    // get all data 
+    while($object_data = $data -> fetch_object()) {
+        echo $object_data -> name . '<br>';
+    }
+
+
 
 
 
