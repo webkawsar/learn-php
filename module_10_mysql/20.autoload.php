2c00f1aea7dd07398b5f1381030191a14879f203
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-03-07 14:32:08 +0600

    practice autoload and data show in html

---
+++ module_10_mysql/20.autoload.php
@@ -0,0 +1,2 @@
+
+// joto function and file sobjaygay lagbe seisob autoload.php file e import korle jekuno file shudhu autoload.php import korle sob eksathe peye jabe
---
+++ module_10_mysql/21.data_show.php
@@ -0,0 +1,2 @@
+
+// get data from database and show data in html
