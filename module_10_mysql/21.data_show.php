14fa1531d75c6e4b8dbd56c3dd3e12f38735d12d
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-03-07 14:54:18 +0600

    practice data show

--- module_10_mysql/21.data_show.php
+++ module_10_mysql/21.data_show.php
@@ -1,2 +1,33 @@
+<?php
 
 // get data from database and show data in html
+
+$host = 'localhost';
+$username = "root";
+$password = '';
+$database = 'b129';
+
+// database connection
+$connection = new mysqli($host, $username, $password, $database);
+
+// 
+$sql = "SELECT * FROM users";
+$data = $connection -> query($sql);
+
+// $user_data = $data -> fetch_assoc();
+
+// echo "<pre>";
+// print_r($user_data);
+// echo "</pre>";
+// echo "<br />";
+
+while($user_data = $data -> fetch_assoc()){
+    echo $user_data['username'] . '<br>';
+}
+
+
+
+
+
+
+
