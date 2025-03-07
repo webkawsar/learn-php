fef5808740c4b5b7fa931c0ce7da27d7855de34c
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-03-07 22:56:50 +0600

    practice mysql command line

---
+++ module_10_mysql/27.mysql_command.php
@@ -0,0 +1,25 @@
+<?php
+
+// login to dashboard
+// mysql -u root -p
+
+
+// show all database
+// show databases;
+
+
+// select one database
+// use databaseName;
+
+
+// show all tables
+// show tables;
+
+
+// get all users from users table
+// select * from users;
+
+
+// get all users from users table by desc
+// select * from users order by id desc;
+
