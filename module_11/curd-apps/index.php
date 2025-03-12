af4a9f175243313b34e899ff4b9ad90a1165d01b
Kawsar <web.kawsarahmed@gmail.com>
2025-03-12 14:28:51 +0600

    added modal in intial template

--- module_11/curd-apps/index.php
+++ module_11/curd-apps/index.php
@@ -30,12 +30,17 @@
 
 		// data insert
 		$sql = "";
-		$connection -> query($sql);
+		// $connection -> query($sql);
 
 
 	?>
 
 	<div class="wrap shadow">
+		<a class="btn btn-sm btn-primary" data-toggle="modal" href="#add_student_modal">Add new student</a>
+		<br>
+		<br>
+
+
 		<div class="card">
 			<div class="card-body">
 				<h2>Sign Up</h2>
@@ -63,6 +68,96 @@
 			</div>
 		</div>
 	</div>
+
+	<!-- Student Create Modal -->
+	<div id="add_student_modal" class="modal fade">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h3>Add new student</h3>
+				</div>
+				<div class="modal-body">
+					<form action="" method="POST" enctype="multipart/form-data">
+
+						<div class="form-group">
+							<label for="">Student Name</label>
+							<input name="name" class="form-control" type="text">
+						</div>
+
+						<div class="form-group">
+							<label for="">Email</label>
+							<input name="email" class="form-control" type="text">
+						</div>
+
+
+						<div class="form-group">
+							<label for="">Cell</label>
+							<input name="cell" class="form-control" type="text">
+						</div>
+
+						<div class="form-group">
+							<label for="">Username</label>
+							<input name="username" class="form-control" type="text">
+						</div>
+
+						<div class="form-group">
+							<label for="">Location</label>
+							<select class="form-control" name="location" id="">
+								<option value="">-select-</option>
+								<option value="Mirpur">Mirpur</option>
+								<option value="Banani">Banani</option>
+								<option value="Uttara">Uttara</option>
+								<option value="Mohammadpur">Mohammadpur</option>
+								<option value="Badda">Badda</option>
+								<option value="Gualshan">Gualshan</option>
+							</select>
+						</div>
+
+						<div class="form-group">
+							<label for="">Age</label>
+							<input name="age" class="form-control" type="text">
+						</div>
+
+						<div class="form-group">
+							<label for="">Gender</label> <br>
+							<input name="gender" type="radio" checked value="Male" id="Male"> <label for="Male">Male</label>
+							<input name="gender" type="radio" value="Female" id="Female"> <label for="Female">Female</label>
+						</div>
+
+						<div class="form-group">
+							<label for="">Dept</label>
+							<select class="form-control" name="dept" id="">
+								<option value="">-select-</option>
+								<option value="BBA">BBA</option>
+								<option value="EEE">EEE</option>
+								<option value="CSE">CSE</option>
+								<option value="English">English</option>
+								<option value="Bangla">Bangla</option>
+								<option value="IT">IT</option>
+							</select>
+						</div>
+
+						<div class="form-group">
+							<label for="">Profile Photo</label> <br>
+							<img id="load_student_photo" style="max-width:100%;width:200px;" src="Profile Picture" alt="">
+							<br>
+							<label for="student_photo"> <img width="100" src="assets/media/img/up.png" alt=""></label>
+							<input id="student_photo" name="photo" style="display:none;" class="form-control" type="file">
+						</div>
+						
+						<div class="form-group">
+							<label for=""></label>
+							<input name="stc" class="btn btn-primary" type="submit" value="add student">
+						</div>
+
+
+
+					</form>
+				</div>
+				<div class="modal-footer"></div>
+			</div>
+		</div>
+	</div>
 	
 
 	<!-- JS FILES  -->
@@ -70,5 +165,13 @@
 	<script src="assets/js/popper.min.js"></script>
 	<script src="assets/js/bootstrap.min.js"></script>
 	<script src="assets/js/custom.js"></script>
+	<script>
+		$('#student_photo').change(function(e) {
+
+			let file_url = URL.createObjectURL(e.target.files[0]);
+			$('#load_student_photo').attr('src', file_url);
+
+		});
+	</script>
 </body>
 </html>
