75f6c0dd84b15e618e1e06e46eec5f53928bc9a3
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-04-10 23:50:04 +0600

    loaded create.php file in index.php file by using ajax

--- module_13/spa/index.php
+++ module_13/spa/index.php
@@ -11,28 +11,21 @@
 </head>
 
 <body>
-
 	<div class="menu">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-12">
 					<a id="all" class="btn btn-primary btn-sm" href="">All Students</a>
-					<a id="add_sudent" class="btn btn-primary btn-sm" href="">Add new student</a>
+					<a id="add_student" class="btn btn-primary btn-sm" href="">Add new student</a>
 					<a id="profile" class="btn btn-primary btn-sm" href="">Profile</a>
 				</div>
 			</div>
 		</div>
 	</div>
 
-
-	<div class="app">
-
-
+	<div id="app">
 
 	</div>
-
-
-
 
 	<!-- JS FILES  -->
 	<script src="assets/js/jquery-3.4.1.min.js"></script>
@@ -41,7 +34,16 @@
 	<script src="assets/js/custom.js"></script>
 	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 	<script>
-		
+		$("#add_student").click(() => {
+			$.ajax({
+				url: "create.php",
+				success: (data) => {
+					$("#app").html(data);
+				}
+			})
+
+			return false;
+		})
 	</script>
 </body>
 
