5d88ba015e6ba71b7139b905e6f01b3b5cf6bcdf
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-04-12 20:42:08 +0600

    student added with image upload

--- module_14/spa/create.php
+++ module_14/spa/create.php
@@ -5,21 +5,30 @@
 				<div class="msg"></div>
 				<form id="student_form" method="POST" enctype="multipart/form-data">
 					<div class="form-group">
+						<label for="">Username</label>
+						<input id="username" name="username" class="form-control" type="text">
+					</div>
+
+					<div class="form-group">
 						<label for="">Name</label>
 						<input id="name" name="name" class="form-control" type="text">
 					</div>
+
 					<div class="form-group">
 						<label for="">Email</label>
 						<input id="email" name="email" class="form-control" type="text">
 					</div>
+
 					<div class="form-group">
 						<label for="">Cell</label>
 						<input id="cell" name="cell" class="form-control" type="text">
 					</div>
+
 					<div class="form-group">
-						<label for="">Username</label>
-						<input id="username" name="username" class="form-control" type="text">
+						<label for="photo">Photo</label>
+						<input id="photo" name="photo" class="form-control" type="file">
 					</div>
+					
 					<div class="form-group">
 						<input id="submit_btn" class="btn btn-primary" type="submit" value="Sign Up">
 					</div>
