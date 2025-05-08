ef70e9b883eb8b9a5b41f01fb0c9b5e33f6f5f71
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-05-08 08:34:41 +0600

    practice oop abstract class, final class, final method

---
+++ oop_module/14.abstract_class-final_class.php
@@ -0,0 +1,58 @@
+<?php
+
+
+// abstract class er kuno object ba instance toiri kora jay na
+// extends kore use korte hoy
+abstract class A {
+    public function food() {
+        echo "I love eat Goru";
+    }
+
+    public final function learn () {
+        echo "Now i learn PHP & WOrdpress";
+    }
+}
+
+// error show kore
+// $a = new A;
+
+class B extends A {
+
+    // public function learn () {
+    //     echo "Now i learn PHP & WOrdpress";
+    // }
+}
+
+// $b = new B;
+// $b->food();
+
+
+final class c {
+    public function learn() {
+        echo "Now i learn PHP & WOrdpress";
+    }
+}
+
+class D extends B {
+
+}
+
+// access kora jabe
+$c = new C;
+$c->learn();
+
+// access kora jabe na
+// $d = new D;
+// $d->learn();
+
+
+// rules
+// abstract class object/instance ber kore use kora jabe na
+// abstract class extends kore use korte hobe
+// final class extends kore use kora jabe na
+// final class er object/instance ber kore use korte hobe
+// final kuno method overrite kora jabe na
+
+
+
+?>
