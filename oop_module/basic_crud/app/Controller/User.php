026eaabcc32bcb3e2e23d2ff969c39ba88893b48
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-05-10 15:27:08 +0600

    data send to database

--- oop_module/basic_crud/app/Controller/User.php
+++ oop_module/basic_crud/app/Controller/User.php
@@ -7,8 +7,8 @@
 class User extends Database {
 
 
-  public function create_user($username, $email, $password) {
-    
+  public function create_user($name, $email, $phone, $username) {
+    parent::create("INSERT INTO users(name, email, phone, username) values('$name', '$email', '$phone', '$username')");
   }
 
 }
