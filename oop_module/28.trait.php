6fc358626f486afae21a8745de75a9fd526b501c
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-05-12 23:58:07 +0600

    practice trait

---
+++ oop_module/28.trait.php
@@ -0,0 +1,56 @@
+<?php
+
+trait Dev {
+
+    public function food(){
+        echo "Dev ra khali khay";
+    }
+
+    public function tour() {
+        echo 'Dev der khali tour dite vala lage';
+    }
+}
+
+trait Help {
+    public function guide() {
+        echo "Dev ra free te Youtube e guide kortese";
+    }
+}
+
+class A {
+
+    public function one() {
+        echo "This is from A class and One method";
+    }
+}
+
+class B {
+
+    public function two() {
+        echo "This is from B class and two method";
+    }
+}
+
+
+class C extends B {
+    
+    use Dev;
+
+
+    public function three() {
+        echo "This is from C class and three method";
+    }
+}
+
+$c = new C;
+// $c->three();
+// $c->two();
+// $c->one();
+// $c->food();
+$c->tour();
+
+
+
+
+
+?>
