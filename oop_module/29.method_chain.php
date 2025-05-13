303098dd147ee54edc7a3ad4c735cafa52c01e11
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-05-13 23:54:20 +0600

    practice method chaining

---
+++ oop_module/29.method_chain.php
@@ -0,0 +1,40 @@
+<?php
+
+    class Student {
+        private $name = "Kawsar Ahmed";
+        private $age = 29;
+        private $profession = "JS Developer";
+
+        public function setName($name) {
+            $this->name = $name;
+            return $this;
+        }
+
+        public function setAge($age) {
+            $this->age = $age;
+            return $this;
+        }
+
+        public function setProfession($profession) {
+            $this->profession = $profession;
+            return $this;
+        }
+
+        public function info() {
+            echo "I am $this->name and I am $this->age years old. I worked as a $this->profession";
+        }
+    }
+
+    $student = new Student;
+    // $student->info();
+
+    // method chain
+    $student->setName("Samim Mohammad")->setAge(30)->setProfession("Pilot")->info();
+
+
+    
+
+
+
+
+?>
