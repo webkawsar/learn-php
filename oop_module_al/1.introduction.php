3908f26efe2396baa3fb28c80a4e3f905c5cca11
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-05-22 23:40:07 +0600

    listening to carefullly introduction lesson

---
+++ oop_module_al/1.introduction.php
@@ -0,0 +1,28 @@
+<?php
+
+
+// Advantages of Object Oriented PHP
+// Re-usability
+// Data redundancy
+// Code maintenance
+// Security
+// Design Benefits
+// Better productivity
+// Easy troubleshooting
+
+
+
+
+// Features of Object Oriented PHP
+// Abstraction
+// Encapsulation
+// Inheritance
+// Polymorphism
+
+
+
+
+
+
+
+
