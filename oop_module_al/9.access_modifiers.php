0d8b01cda1f70f5ab1e8a3f5491924523062c15b
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-05-31 23:32:21 +0600

    practice oop access modifiers

---
+++ oop_module_al/9.access_modifiers.php
@@ -0,0 +1,10 @@
+<?php
+
+// public jinis sob jayga theke access kora jabe
+// private jinis shudu oi class er vitore use kora jabe
+// protected jinis shudu oi class and extends class er maje use kora jabe
+// kuno function er samne kuno access modifiers na dile by default oi function public
+// private jinis extends class theke change kora jabe na protected jinis change kora jabe extends class thekw
+
+
+?>
