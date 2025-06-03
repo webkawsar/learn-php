e94b4404bc77445e17bc5626c9bc28ff889fa40d
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-06-03 23:55:47 +0600

    practice method overriding

--- oop_module_al/10.method_overriding.php
+++ oop_module_al/10.method_overriding.php
@@ -1,6 +1,7 @@
 <?php
 
 class One {
+    public $name = "Kawsar Ahmed";
     public function display() {
         echo "This is show from One class display method <br>";
     }
@@ -9,11 +10,15 @@ class One {
 class Two extends One {
     public function display() {
         echo "This is show from Two class display method <br>";
+        parent::display();
+        $this->name = "Samim Mohammed";
+        echo "Name is: {$this->name} <br>";
     }
 }
 
 $two = new Two();
 $two->display();
+echo $two->name;
 
 
 
